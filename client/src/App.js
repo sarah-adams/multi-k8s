d617aecaaf3974c9c65c19393bf15dd1e8b98233
Sarah Adams <sarah.adams@redspace.com>
2026-04-20 15:39:00 -0300

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import OtherPage from "./OtherPage";
-import Fib from "./Fib";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
 
 function App() {
   return (
@@ -11,7 +11,6 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello</p>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -24,10 +23,6 @@ function App() {
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
-          {/* <Routes>
-            <Route exact path="/" element={<Fib />} />
-            <Route path="/otherpage" element={<OtherPage />} />
-          </Routes> */}
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
         </div>
